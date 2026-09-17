73f7da300f4e1da2e0bc7e619eb4b69e98f7cea6
marcelomartinura <urangamarcelo74@gmail.com>
2026-09-17 07:50:39 -0600

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import CatalogoBuscador from "@/components/CatalogoBuscador";
 import FeaturedVehicles from "@/components/FeaturedVehicles";
+import ClasificadosDestacados from "@/components/ClasificadosDestacados";
+import EcosistemaCrossSell from "@/components/EcosistemaCrossSell";
+import ComoComprar from "@/components/ComoComprar";
+import PreguntasFrecuentes from "@/components/PreguntasFrecuentes";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
+      <CatalogoBuscador />
       <FeaturedVehicles />
+      <ClasificadosDestacados />
+      <EcosistemaCrossSell />
+      <ComoComprar />
+      <PreguntasFrecuentes />
     </main>
   );
 }
